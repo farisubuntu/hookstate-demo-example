16ca6fc12d0c829259ad161ebd5e5ed97d89fda0
farisubuntu <farisubuntu@gmail.com>
2025-04-07 02:13:06 +0300

    asdf

---
+++ src/components/demo/notifications-store.ts
@@ -0,0 +1,14 @@
+
+export type Notification = {
+  id: string;
+  type: 'info' | 'warning' | 'success' | 'error';
+  title: string;
+  message?: string;
+};
+
+type NotificationsStore = {
+  notifications: Notification[];
+  addNotification: (notification: Omit<Notification, 'id'>) => void;
+  dismissNotification: (id: string) => void;
+};
+
